559be051e55343a220ecfc8d5750990dff080d49
lucas_almeida <lucas.almdiniz@gmail.com>
2023-02-12 18:13:17 -0300

    feat: criada rota "POST /users/new-user"

--- api_rest/urls.py
+++ api_rest/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("", views.get_all_users, name="get_all_users"),
-    path("/<int:id>", views.update_user),
+    path("/new-user", views.create_user, name="create_user"),
+    path("/<int:id>", views.update_user, name="update_user"),
 ]
