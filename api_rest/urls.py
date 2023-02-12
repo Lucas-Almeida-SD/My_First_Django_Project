c2c4ecacf899477afe625cc1a224c670e6227da7
lucas_almeida <lucas.almdiniz@gmail.com>
2023-02-12 19:15:45 -0300

    refactor: função "update_user" alterada para "get_user_by_id"

--- api_rest/urls.py
+++ api_rest/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path("", views.get_all_users, name="get_all_users"),
     path("/new-user", views.create_user, name="create_user"),
-    path("/<int:id>", views.update_user, name="update_user"),
+    path("/<int:id>", views.get_user_by_id, name="update_user"),
 ]
