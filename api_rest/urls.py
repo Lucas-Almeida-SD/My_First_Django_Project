8830ca681d948d96738d72ecbc25d55968aa9ce2
lucas_almeida <lucas.almdiniz@gmail.com>
2023-02-12 16:52:22 -0300

    feat: criada rota "PUT /users/{id}"

--- api_rest/urls.py
+++ api_rest/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.get_all_users, name="get_all_users")]
+urlpatterns = [
+    path("", views.get_all_users, name="get_all_users"),
+    path("/<int:id>", views.update_user),
+]
