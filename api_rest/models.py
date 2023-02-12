b45156ff61c78adaae7bce445e4b8ffd9ece9e2d
lucas_almeida <lucas.almdiniz@gmail.com>
2023-02-11 23:30:16 -0300

    feat: criado model User

--- api_rest/models.py
+++ api_rest/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    id = models.AutoField(primary_key=True)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(unique=True, null=False)
+
+    def __str__(self):
+        return (
+            f"id: {self.id}"
+            f"first_name: {self.first_name}"
+            f"last_name: {self.last_name}"
+            f"email: {self.email}"
+        )
